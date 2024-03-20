1bf62b4e2dab5cc2fc464dc6d3605d6453936308
kilicsumeyra <sumeyra.k_53@icloud.com>
2024-03-20 15:30:08 +0300

    language

--- src/components/Language.jsx
+++ src/components/Language.jsx
@@ -4,27 +4,27 @@ import Image from "react-bootstrap/Image";
 
 const Language = ({ name, img, options }) => {
   // const{name, img, options}=lang; //destr
-const[showDesc , setShowDesc]= useState(false)
+  const [showDesc, setShowDesc] = useState(false);
 
   return (
-    <Container className="rounded-3 p-4 h-100 lang-card"
-     style={{ backgroundColor: "lavender" }}
-     onClick={()=> setShowDesc(!showDesc)}>
-{! showDesc && (
-   <Container>
-    <Image src={img} width="70%" className="lang-logo" />
-      <h3 className="display-6">{name}</h3>
-</Container> 
-)}
-{showDesc &&( 
-    <ol className= "h-100 d-flex flex-column justify-content-center">
-{options.map((item)=>(
-    <li>{item}</li>
-))}
-</ol>
-)}
-
-
+    <Container
+      className="rounded-3 p-4 h-100 lang-card"
+      style={{ backgroundColor: "lavender" }}
+      onClick={() => setShowDesc(!showDesc)}
+    >
+      {!showDesc && (
+        <Container>
+          <Image src={img} width="70%" className="lang-logo" />
+          <h3 className="display-6">{name}</h3>
+        </Container>
+      )}
+      {showDesc && (
+        <ol className="h-100 d-flex flex-column justify-content-center">
+          {options.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ol>
+      )}
     </Container>
   );
 };
